58481953b4f2a870ac9ddf92283c3693938f6754
LubnaAbidah <LubnaAbidah>
2022-06-06 23:31:49 +0700

    edit function sorter index.js

--- index.js
+++ index.js
@@ -6,18 +6,36 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (namesArray, orderBy) => {
+  let namesArr = orderBy(namesArray);
+  let arr = [];
+  let resultSort = [];
+  let sort = 0;
+  for (let i = 0; i < namesArr.length; i++)
+  {
+      arr[i] = namesArr[i];
+      sort += 1;
+      
+      resultSort[i] = sort+". "+arr[i];
+  }
+  return resultSort;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (namesArr) => {
+   return namesArr.sort()
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (namesArr) => {
+  return namesArr.reverse()
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
+  console.log(sorter)?.join("\n")
   console.log(sorter?.(names, sortAscending)?.join("\n"));
   console.log(sorter?.(names, sortDescending)?.join("\n"));
 })();
